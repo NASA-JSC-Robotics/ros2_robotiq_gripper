c2c01bab1ba4e19ace0eae47a0388f2dac6abc10
Sharp, Andrew P 527152135 <andrew.p.sharp@nasa.gov>
2023-05-11 21:47:15 +0000

    Hande Integration

---
+++ robotiq_driver/include/robotiq_driver/hande_hardware_interface.hpp
@@ -0,0 +1,28 @@
+
+#pragma once
+
+#include "robotiq_driver/robotiq_gripper_interface.hpp"
+#include "hardware_interface/system_interface.hpp"
+
+namespace robotiq_driver_hande
+{
+class RobotiqGripperHandeHardwareInterface: public robotiq_driver::RobotiqGripperHardwareInterface
+{
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(RobotiqGripperHandeHardwareInterface)
+
+  ROBOTIQ_DRIVER_PUBLIC
+  RobotiqGripperHandeHardwareInterface();
+
+  ROBOTIQ_DRIVER_PUBLIC
+  hardware_interface::return_type read(const rclcpp::Time& time, const rclcpp::Duration& period) override;
+
+  ROBOTIQ_DRIVER_PUBLIC
+  hardware_interface::return_type write(const rclcpp::Time& time, const rclcpp::Duration& period) override;
+
+protected:
+  std::unique_ptr<RobotiqGripperInterface> gripper_interface_;
+
+};
+
+}  // namespace robotiq_driver_hande
